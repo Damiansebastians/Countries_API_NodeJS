dcaa20a04a998a7aff621c9bea3fb197d57373d2
Damiansebastians <116028887+Damiansebastians@users.noreply.github.com>
2023-07-24 15:25:31 +0200

    working in script

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ const fs = require('fs'); // file system
 const path = require('path'); // routes absolute
 
 interface CountryData {
-  country: string;
+  name: string;
   population: number;
   area: number;
-  density?: number
+  density: number
 }
 
 //-------------------------------
@@ -16,56 +16,63 @@ const readData = () => {
   try {
     const pathCountries = path.join(__dirname, 'data/countries.txt');
     const countries: string = fs.readFileSync(pathCountries, 'utf-8');
+
     const lines = countries.split('\n');
 
     for (const line of lines) {
-      const regex = /^(.*?)\s+([\d,]*)\s+([\d,]*)$/;
-      const countryDataMatches = line.match(regex);
-
-      if (countryDataMatches) {
-        const [, country, population, area] = countryDataMatches;
-
-        countriesData.push({
-          country: country.trim(),
-          population: parseInt(population.replace(/,/g, '')),
-          area: parseInt(area.replace(/,/g, '')),
-        });
+      const parts = line.split(' ');
+      const len = parts.length;
+      if (len === 2) {
+        throw new Error('Line too short or incorrect');
       }
+
+      const name = parts.slice(0, len - 2).join(' ');
+      const population = parseInt(parts[len - 2].replaceAll(',', ''));
+      const area = parseInt(parts[len - 1].replaceAll(',', ''));
+      const density = (population/area);
+      
+      countriesData.push({ name, population, area, density });
     }
   } catch (error: any) {
     console.error('Error reading data:', error.message);
   }
-
+  
   return countriesData;
 };
 
-
-
 const countriesData = readData();
 
-function calculateDensity(countriesData: CountryData[]){
 
-  return countriesData.map(countryData => ({
-    country: countryData.country,
-    population: countryData.population,
-    area: countryData.area,
-    density: countryData.population / countryData.area,
-  }));
-}
+    // const lines = countries.split('\n');
 
-const densities = calculateDensity(countriesData);
+    // for (const line of lines) {
+    //   const regex = /^(.*?)\s+([\d,]*)\s+([\d,]*)$/;
+    //   const countryDataMatches = line.match(regex);
+
+    //   if (countryDataMatches) {
+    //     const [, country, population, area] = countryDataMatches;
+
+    //     countriesData.push({
+    //       country: country.trim(),
+    //       population: parseInt(population.replace(/,/g, '')),
+    //       area: parseInt(area.replace(/,/g, '')),
+    //     });
+    //   }
+    // }
+
+
 
 //--------------Ordenar
-densities.sort((a, b) => b.density - a.density);
+countriesData.sort((a, b) => b.density - a.density);
 
 //---------------------
-densities.forEach(data => {
-  console.log(`${data.country}: Density = ${data.density.toFixed(2)} people/km²`);
+countriesData.forEach(data => {
+  console.log(`${data.name}: Density = ${data.density.toFixed(2)} people/km²`);
 });
 
 
-const csvCountries = 'country,population,area,density\n' + densities.map(data => {
-  return `${data.country},${data.population},${data.area},${data.density.toFixed(2)}`;
+const csvCountries = 'country,population,area,density\n' + countriesData.map(data => {
+  return `${data.name},${data.population},${data.area},${data.density.toFixed(2)}`;
 }).join('\n');
 
 
